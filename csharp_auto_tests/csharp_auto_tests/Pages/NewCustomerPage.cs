e6aef734ccd0b78abb0ff3ba9931b946750f81ff
AlexWorkQA <alexWorkQA@gmail.com>
2018-08-28 19:11:02 +0300

    Add Pages

--- csharp_auto_tests/csharp_auto_tests/Pages/NewCustomerPage.cs
+++ csharp_auto_tests/csharp_auto_tests/Pages/NewCustomerPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using csharp_auto_tests.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace csharp_auto_tests.Pages
 {
-    class NewCustomerPage
+    class NewCustomerPage:BasePage,IPage
     {
+
     }
 }
